02610d6a5564febe4fdea256fa9ba6819d332d75
Rafael Carvalho Itajahy <rafaelitajahy@gmail.com>
2019-08-21 09:30:21 -0300

    Buscando propriedade de um objeto em array

--- dominando-array.js
+++ dominando-array.js
@@ -6,26 +6,26 @@ function properties(caption, property) {
 
 //SintaxeSeção
 var names = [
-  "Rafael",
-  "Mariane",
-  "Elisa",
-  "Gabriel",
-  "Cleusa",
-  "Janer",
-  "Flávio",
-  "Alinne",
-  "Thaiane",
-  "Tom"
+  'Rafael',
+  'Mariane',
+  'Elisa',
+  'Gabriel',
+  'Cleusa',
+  'Janer',
+  'Flávio',
+  'Alinne',
+  'Thaiane',
+  'Tom'
 ];
 
-var fruits = new Array("Maçã", "Laranja");
-properties("Array", fruits);
+var fruits = new Array('Maçã', 'Laranja');
+properties('Array', fruits);
 
 var cars = new Array(5);
-properties("Array", cars);
+properties('Array', cars);
 
 //array size
-properties("Length", names.length);
+properties('Length', names.length);
 
 //Acessar um item (index) do Array
 var name = names[0];
@@ -38,44 +38,62 @@ names.forEach(function(item, indice, array) {
 });
 
 //Adicionar um item ao final do Array
-names.push("Adair");
-properties("Array", names);
+names.push('Adair');
+properties('Array', names);
 
 //Remover um item do final do Array
 names.pop();
-properties("Array", names);
+properties('Array', names);
 
 //Remover do início do Array
 names.shift();
-properties("Array", names);
+properties('Array', names);
 
 //Adicionar ao início do Array
-names.unshift("Adair");
-properties("Array", names);
+names.unshift('Adair');
+properties('Array', names);
 
 //Procurar o índice de um item na Array
-properties("IndexOf", names.indexOf("Mariane"));
+properties('IndexOf', names.indexOf('Mariane'));
 
 //Remover um item pela posição do índice
-properties("Splice", names.splice(0, 1));
+properties('Splice', names.splice(0, 1));
 
 //Copiar um Array
 var copyNames = names.slice();
-properties("Slice", copyNames);
+properties('Slice', copyNames);
 
 //Exibir elementos
-properties("Object.keys", Object.keys(names));
+properties('Object.keys', Object.keys(names));
 
 //object
 
 var people = {
-  name: "Rafael",
+  name: 'Rafael',
   array: [1, 2, 3, 4]
 };
-properties("Object", people["name"]);
-properties("Object", people["array"]);
+properties('Object', people['name']);
+properties('Object', people['array']);
 
 //RegExp
 var minhaRegex = /d(b+)(d)/i;
-var meuArray = minhaRegex.exec("cbdBdbsbz");
-properties("Arry RegExp", meuArray);
+var meuArray = minhaRegex.exec('cbdBdbsbz');
+properties('Arry RegExp', meuArray);
+
+//buscando propriedade objeto em array
+
+var smartphones = [
+  {
+    id: '1',
+    marca: 'Samsung',
+    modelo: 'S10'
+  },
+  {
+    id: '2',
+    marca: 'Motorola',
+    modelo: 'Moto X4'
+  }
+];
+
+var smartphone = smartphones.find(s => s.id == '1');
+console.log(smartphone);
